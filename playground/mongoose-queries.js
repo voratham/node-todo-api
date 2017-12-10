5274010384c440320aec4180d4fa7847d49b5bd3
voratham <dream07312@gmail.com>
2017-12-10 23:44:35 +0700

    Add TEST GET /todos and Add queries script on playground

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,39 @@
+const {ObjectID} = require('mongodb');
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+var id = '5a2d5deed1c15dcfc0c3063c';
+
+// if(!ObjectID.isValid(id)){
+//     console.log('ID not valid');
+// }
+
+// Todo.find({
+//     _id : id
+// }).then( (todos) => {
+//     console.log('Todos ' , todos);
+// })
+
+
+
+// Todo.findOne({_id: id})
+//     .then((todo) => {
+//         console.log('----------------');
+//         console.log('Todo ', todo);
+// })
+
+
+// Todo.findById(id).then( res => {
+//     if(!res){
+//         return console.log('Id not found !');  
+//     }
+//     console.log('Todo by id :: ' , res)
+// }).catch( err => console.log(err));
+
+
+User.findById("5a2d2c1f3decea0667ea9569").then( user => {
+    if(!user) return console.log('User not found !');
+    console.log(JSON.stringify(user , undefined , 2));
+}).catch( err => console.log('err :: ' , err))
+
--- server/server.js
+++ server/server.js
@@ -15,13 +15,21 @@ app.post('/todos', (req ,res ,next) => {
     var todo = new Todo({
          text : req.body.text
     })
-    todo.save().then( (result) => {
-        res.send(result)
+    todo.save().then( (todos) => {
+        res.send(todos)
     } , (e) => {
         res.status(400).send(e)
     })
 })
 
+
+app.get('/todos' , (req ,res) => {
+    Todo.find().then( (todos) => {
+        res.send({todos})
+    } , (e) => {
+        res.status(400).send(e)        
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`Start Server on port ${PORT}`)
